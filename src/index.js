6b4dbfef021243be9efd4287785fee722c82fc47
Cristina Aguilera <developer.c.93.c@gmail.com>
2024-09-07 00:06:49 +0200

    lab done

--- src/index.js
+++ src/index.js
@@ -6,29 +6,55 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
+const buttonNode = document.querySelector("button")
+buttonNode.addEventListener("click",()=>{
+
+  startCountdown()
+})
 
 
 
 
 // ITERATION 2: Start Countdown
+const divTimeNode = document.querySelector("#time")
+
 function startCountdown() {
-  console.log("startCountdown called!");
-
-
-  // Your code goes here ...
+  console.log("startCountdown called!")
+  
+  const intervalId = setInterval(()=>{
+    remainingTime--
+    buttonNode.disabled=true
+    divTimeNode.innerText=remainingTime
+    
+    if(remainingTime===0){
+      
+      clearInterval(intervalId)
+      
+      buttonNode.disabled=false
+      showToast( "Lift off! 🚀")
+    }
+    
+  }, 1000)
+  remainingTime = DURATION;
+  divTimeNode.innerText = remainingTime;
+  
 }
 
 
 
 
 // ITERATION 3: Show Toast
+const toastHiddenNode = document.querySelector(".toast")
+const messageNode = document.querySelector("#toast-message")
+
 function showToast(message) {
-  console.log("showToast called!");
-
-  // Your code goes here ...
-
-
+  
+  toastHiddenNode.classList.add("show")
+  messageNode.innerText = message
+  setTimeout(() => {
+    toastHiddenNode.classList.remove("show");
+  }, 2000);
+  
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
